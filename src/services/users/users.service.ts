241e94fc2ea99b83a8bc4ef8d606101a16ee1d8d
Gibunn <abgibun@gmail.com>
2026-07-23 10:23:47 +0700

    chore: implementing hash with bcrypt in create user

--- src/services/users/users.service.ts
+++ src/services/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Prisma } from 'generated/prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserDTO } from 'src/dto/users.dto';
+import { hashPassword } from 'src/utils/hash.utils';
 
 @Injectable()
 export class UsersService {
@@ -16,10 +17,12 @@ export class UsersService {
     }
 
     async createUser(data: CreateUserDTO) {
+        const hashedPassword = await hashPassword(data.password)
+
         const formatData = {
             email: data.email,
             username: data.username,
-            password: data.password,
+            password: hashedPassword,
             avatar_url: '',
             provider_id: '',
             auth_provider: ''
